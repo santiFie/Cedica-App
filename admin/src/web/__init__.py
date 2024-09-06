1fcfac833f0781f4973cedae8936fecd09cf2ee4
Manuel Dias Lourenco <manueldiasuala@gmail.com>
2024-09-06 12:31:34 -0300

    Se agrega vista de error 404.

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask import render_template
+from src.web.handlers import error
 
 def create_app(env="development", static_folder="../../static"):
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder= static_folder)
 
     @app.route("/")
     def home():
         return render_template("home.html")
+    
+    app.register_error_handler(404, error.not_found_error)
     
     return app
