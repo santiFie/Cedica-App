0133424235e6d4ce452071514092a5601825ae50
Julian-Garcia <julian020404@gmail.com>
2024-09-06 12:08:33 -0300

    Se agrego el base.html, se modifico el init.py y se agrego css

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
-def create_app(env="development", static_folder=""):
+def create_app(env="development", static_folder="../../static"):
     app = Flask(__name__)
 
     @app.route("/")
     def home():
-        return "Hola Mundo!"
+        return render_template("home.html")
     
     return app
