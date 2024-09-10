7e52bd250319bd75d676aa41e35d6a87c5afdeb2
luciocolli <luciocolli1@gmail.com>
2024-09-10 17:01:30 -0300

    Se agrega manejador de error 500

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, abort
 from flask import render_template
 from src.web.handlers import error
 
@@ -11,4 +11,12 @@ def create_app(env="development", static_folder="../../static"):
     
     app.register_error_handler(404, error.not_found_error_404)
     
+    # queda ruta para chequear que esta bien
+    @app.route("/error500")
+    def error500():
+        abort(500)
+
+    app.register_error_handler(500, error.server_error_500)
+
+
     return app
--- admin/src/web/handlers/error.py
+++ admin/src/web/handlers/error.py
@@ -13,3 +13,8 @@ def not_found_error_404(e):
     error = Error(404, "404 - Página no encontrada", "Página no encontrada", "La página que estás buscando no existe", "Verifica la URL que ingresaste o elige una opción debajo")
 
     return render_template("error.html", error=error), 404
+
+def server_error_500(e):
+    error = Error(500, "500 - Error interno del servidor", "Error interno del servidor", "Ocurrio un error inesperado en el servidor", "Intenta nuevamente mas tarde")
+
+    return render_template("error.html", error=error), 500
