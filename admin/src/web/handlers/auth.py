d7e3579ad1d1224907b43fdefefda4eb48bd7321
SANTIAGO FIERRO <santiago.fierro99@gmail.com>
2024-09-27 11:20:15 -0300

    Se agrega login_required

---
+++ admin/src/web/handlers/auth.py
@@ -0,0 +1,17 @@
+from functools import wraps
+from flask import session
+from flask import abort
+
+def is_authenticated(session):
+    return session.get("user") is not None
+
+def login_required(func):
+
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        if not is_authenticated(session):
+            abort(401)
+
+        return func(*args, **kwargs)
+    
+    return decorated_function
--- admin/src/web/routes.py
+++ admin/src/web/routes.py
@@ -1,8 +1,11 @@
 from src.web.controllers.auth import bp as auth_bp
 from flask import render_template
+from src.web.handlers.auth import login_required
 
 
 def register(app):
+
+    @login_required
     @app.route("/")
     def home():
         return render_template("home.html")
