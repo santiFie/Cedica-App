a5327ec5e13058b5772425d060e46b64f90cd8a1
SANTIAGO FIERRO <santiago.fierro99@gmail.com>
2024-09-28 17:02:37 -0300

    Se agregan los modelos Role, Permission y RolePermission

--- admin/src/core/models/users.py
+++ admin/src/core/models/users.py
@@ -8,9 +8,42 @@ class User(database.db.Model):
     password = database.db.Column(database.db.String(120), nullable=False)
     active = database.db.Column(database.db.Boolean, default=True)
     system_admin = database.db.Column(database.db.Boolean, default=False)
-    #role_id = database.db.Column(database.db.Integer, database.db.ForeignKey("role.id"), default=1)
-    iserted_at = database.db.Column(database.db.DateTime, default=datetime.now())
+    role_id = database.db.Column(database.db.BigInteger, database.db.ForeignKey('roles.id'), nullable=False)
+    inserted_at = database.db.Column(database.db.DateTime, default=datetime.now())
     updated_at = database.db.Column(database.db.DateTime, default=datetime.now(), onupdate=datetime.now())
+    
+    # Relación con roles
+    role = database.db.relationship('Role', back_populates='users')
 
-    def __repr__(self):
-        return f"<Usuario {self.email}>"
+
+# Modelo Role
+class Role(database.db.Model):
+    __tablename__ = 'roles'
+    
+    id = database.db.Column(database.db.BigInteger, primary_key=True)
+    name = database.db.Column(database.db.String, nullable=False)
+    
+    # Relación con usuarios
+    users = database.db.relationship('User', back_populates='role')
+    
+    # Relación con permisos a través de RolePermission
+    permissions = database.db.relationship('Permission', secondary='role_permissions', back_populates='roles')
+
+
+# Modelo Permission
+class Permission(database.db.Model):
+    __tablename__ = 'permissions'
+    
+    id = database.db.Column(database.db.BigInteger, primary_key=True)
+    name = database.db.Column(database.db.String, nullable=False)
+    
+    # Relación con roles a través de RolePermission
+    roles = database.db.relationship('Role', secondary='role_permissions', back_populates='permissions')
+
+
+# Modelo RolePermission (Tabla intermedia)
+class RolePermission(database.db.Model):
+    __tablename__ = 'role_permissions'
+    
+    role_id = database.db.Column(database.db.BigInteger, database.db.ForeignKey('roles.id'), primary_key=True)
+    permission_id = database.db.Column(database.db.BigInteger, database.db.ForeignKey('permissions.id'), primary_key=True)
