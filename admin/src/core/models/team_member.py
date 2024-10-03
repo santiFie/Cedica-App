d8cad64b22a3dae5e0302ba08c22ccf2d8048f73
SANTIAGO FIERRO <santiago.fierro99@gmail.com>
2024-10-03 15:32:00 -0300

    Se cambian campos String por Enum

--- admin/src/core/models/team_member.py
+++ admin/src/core/models/team_member.py
@@ -1,4 +1,45 @@
 from src.core import database
+from enum import Enum
+
+class ProfessionEnum(Enum):
+        PSYCHOLOGIST = "Psicólogo/a"
+        PSYCHOMOTRICIAN = "Psicomotricista"
+        DOCTOR = "Médico/a"
+        PHYSIOTHERAPIST = "Kinesiólogo/a"
+        OCCUPATIONAL_THERAPIST = "Terapista Ocupacional"
+        EDUCATIONAL_PSYCHOLOGIST = "Psicopedagogo/a"
+        TEACHER = "Docente"
+        PROFESSOR = "Profesor"
+        SPEECH_THERAPIST = "Fonoaudiólogo/a"
+        VETERINARIAN = "Veterinario/a"
+        OTHER = "Otro"
+
+        def __str__(self):
+            return self.value
+        
+class Job(Enum):
+    ADMINISTRATIVE = "Administrativo/a"
+    THERAPIST = "Terapeuta"
+    DRIVER = "Conductor"
+    TRACK_ASSISTANT = "Auxiliar de pista"
+    BLACKSMITH = "Herrero"
+    VETERINARIAN = "Veterinario"
+    HORSE_TRAINER = "Entrenador de Caballos"
+    HORSE_TAMER = "Domador"
+    EQUESTRIAN_TEACHER = "Profesor de Equitación"
+    TRAINING_TEACHER = "Docente de Capacitación"
+    MAINTENANCE_ASSISTANT = "Auxiliar de mantenimiento"
+    OTHER = "Otro"
+
+    def __str__(self):
+        return self.value
+    
+class Condition(Enum):
+    VOLUNTEER = "Voluntario"
+    PAID_STAFF = "Personal Rentado"
+
+    def __str__(self):
+        return self.value
 
 class TeamMember(database.db.Model):
     __tablename__ = "team_members"
@@ -9,16 +50,16 @@ class TeamMember(database.db.Model):
     email = database.db.Column(database.db.String(120), nullable=False, unique=True)
     locality = database.db.Column(database.db.String(120), nullable=False)
     phone = database.db.Column(database.db.String(120), nullable=False)
-    proffesion = database.db.Column(database.db.String(120), nullable=False)
-    job_position = database.db.Column(database.db.String(120), nullable=False)
     initial_date = database.db.Column(database.db.DateTime, nullable=False)
     end_date = database.db.Column(database.db.DateTime, nullable=True)
     emergency_contact = database.db.Column(database.db.String(120), nullable=False)
     emergency_phone = database.db.Column(database.db.String(120), nullable=False)
-    condition = database.db.Column(database.db.String(120), nullable=False)
     active = database.db.Column(database.db.Boolean, nullable=False, default=True)
     health_insurance_id = database.db.Column(database.db.Integer, database.db.ForeignKey('health_insurances.id'), nullable=False)
 
+    condition = database.db.Column(database.db.Enum(Condition), nullable=False)
+    job_position = database.db.Column(database.db.Enum(Job), nullable=False)
+    proffesion = database.db.Column(database.db.Enum(ProfessionEnum), nullable=False)
     health_insurance = database.db.relationship('HealthInsurance', back_populates='team_members')
 
     def __repr__(self):
