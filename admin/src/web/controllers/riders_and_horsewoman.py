050d7c4bd52dd8decc9fc25228cc0b3be061d725
manudiaslourenco <manueldiasuala@gmail.com>
2024-10-15 03:37:52 +0000

    Se sigue con el formulario  de jinetes/amazonas.

--- admin/src/web/controllers/riders_and_horsewoman.py
+++ admin/src/web/controllers/riders_and_horsewoman.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from src.core.models.riders_and_horsewomen import disability_certificate_enum, disability_type_enum, family_allowance_enum, pension_enum, days_enum, condition_enum, seat_enum, proposal_enum, education_level_enum 
 from src.core import riders_and_horsewomen as rh
 from src.core import health_insurance as hi
@@ -12,22 +12,34 @@ bp = Blueprint('riders_and_horsewomen',__name__,url_prefix="/riders_and_horsewom
 def riders_and_horsewomen_list():
     return 
 
-@bp.get("/new")
+@bp.route("/new", methods=["GET", "POST"])
 @login_required
 def new():
     disability_certificate_options = disability_certificate_enum.enums
     disability_type_options = disability_type_enum.enums
     family_allowance_options = family_allowance_enum.enums
     pension_options = pension_enum.enums
-    return render_template("riders_and_horsewomen/new1.html", disability_certificate_options=disability_certificate_options, disability_type_options=disability_type_options, family_allowance_options=family_allowance_options, pension_options=pension_options)
+    
+    #donde van funciones aux de checkeo de datos.
+    if request.method == "POST":
+        rider = rh.find_rider(request.form["dni"])
 
-@bp.get("/new/tutor")
+        if not rider:
+            #se checkean todos los campos
+            flash("El jinete/Amazona se creado exitosamente")
+        else:
+            flash("El dni ingresado ya existe", "info")
+    
+    
+    return render_template("riders_and_horsewomen/new.html", disability_certificate_options=disability_certificate_options, disability_type_options=disability_type_options, family_allowance_options=family_allowance_options, pension_options=pension_options)
+
+@bp.route("/new/tutor", methods=["GET", "POST"])
 @login_required
 def new_tutor():
     education_level_options = education_level_enum.enums
     return render_template("riders_and_horsewomen/new_tutor.html", education_level_options= education_level_options)
 
-@bp.get("/new/institution")
+@bp.route("/new/institution", methods=["GET", "POST"])
 @login_required
 def new_institution():
     return render_template("riders_and_horsewomen/new_institution.html")
