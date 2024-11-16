490dfbcc6e3fd75790146068f751f58533541fe1
SANTIAGO FIERRO <santiago.fierro99@gmail.com>
2024-11-16 15:57:46 -0300

    Se realiza listado de posts con paginación

--- portal/src/components/AllPosts.vue
+++ portal/src/components/AllPosts.vue
@@ -4,10 +4,18 @@
     import { onMounted } from 'vue'
 
     const postsStore = usePostsStore()
-    const { posts, loading, error } = storeToRefs(postsStore)
+    const { posts, loading, error, total_pages, page } = storeToRefs(postsStore)
 
     const fetchPosts = async () => {
         await postsStore.fetchPosts()
+    }
+
+    const changePage = async () => {
+        await postsStore.changePage()
+    }
+
+    const goBackPage = async () => {
+        await postsStore.goBackPage()
     }
 
     onMounted(() => {
@@ -23,27 +31,46 @@
     <div class="wrapper">
         <div class="column1">
             <h2>Publicaciones y Actividades</h2>
-            <p v-if="loading">Cargando...</p>
-            <p v-else-if="error">Error: {{ error }}</p>
-            <p v-else-if="!posts.length">No hay publicaciones</p>
+            <p v-if="error">Error: {{ error }}</p>
         </div>
-        <div v-if="posts.length>0" class="column2">
-            <ul>
-                <li v-for="post in posts" :key="post.id">
+        <div class="column2">
+            <ul v-if="posts.length > 0">
+                <li v-for="post in posts" :key="post.id" class="post-item">
                     <h3 class="post-title">{{ post.title }}</h3>
                     <p class="post-summary">{{ post.summary }}</p>
                     <p class="post-posted_at">{{ post.posted_at }}</p>
-                    <form action="/post" method="post">
+                    <form action="/post" method="post" >
                         <input type="hidden" name="id" :value="post.id">
-                        <button type="submit">Ver más</button>
+                        <button type="submit" class="post-button">Ver más</button>
                     </form>
                 </li>
             </ul>
+            
+            <p v-else>No hay publicaciones</p>
+            <p v-if="loading">Cargando...</p>
+            <div class="page-counter">
+                <div>
+                    <button v-if="page > 1" @click="goBackPage()" class="page-button">Anterior</button>
+                </div>
+                <div>
+                    <p :style="{ fontSize: '1.3rem' }">Página {{ page }} de {{ total_pages }}</p>
+                </div>
+                <div>
+                    <button v-if="page < total_pages" @click="changePage()" class="page-button">Siguiente</button>
+                </div>
+            </div>
         </div>
+        
     </div>
 </template>
 
 <style scoped>
+
+    p{
+        color: black;
+        font-size: 1.5rem;
+        text-align: center;
+    }
 
     h2 {
         color: black;
@@ -62,16 +89,25 @@
 
     .column2 {
         flex: 3;
-        padding: 20px;
-
+        padding: 2%;
+        background-color: #ffffff;
+        border-radius: 10px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
     }
 
-    li{
+    .post-item{
         margin-bottom: 20px;
         padding: 10px;
-        border: 1px solid black;
+        border: 1px solid #f5f5f5;
         border-radius: 5px;
         background-color: #f5f5f5;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1); /* Sombras para dar profundidad */
+        transition: transform 0.2s ease, b; /* Animación al hacer hover */
+    }
+
+    .post-item:hover {
+        transform: scale(1.02); /* Efecto de zoom */
+        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2); /* Mayor sombra al hacer hover */
     }
 
     .post-title {
@@ -86,6 +122,8 @@
         margin-bottom: 10px;
         text-align: justify;
         color: black;
+        padding-left: 1%;
+        padding-right: 1%;
     }
 
     .post-posted_at {
@@ -93,6 +131,48 @@
         margin-bottom: 10px;
         text-align: right;
         color: black;
+    }
+
+    .post-button {
+        font-size: large;
+        font-family: inherit;
+        margin: auto;
+        margin-left: 40px;
+        padding: 0.8%;
+        background-color: lightblue;
+        border: none;
+        border-radius: 5px; /* Added to make the button rounded */
+        display: block;
+        cursor: pointer;
+        transition: background-color 0.3s ease, transform 0.3s ease, box-shadow 0.3s ease;
+    }
+
+    .post-button:hover {
+        background-color: #58b7ee; /* Azul más oscuro */
+        transform: translateY(-2px); /* Efecto de elevación */
+    }
+
+    .page-button {
+        font-size: large;
+        font-family: inherit;
+        margin: 1%;
+        padding: 14%;
+        background-color: lightblue;
+        border: none;
+        border-radius: 5px; /* Added to make the button rounded */
+        display: block;
+        cursor: pointer;
+        transition: background-color 0.3s ease, transform 0.3s ease, box-shadow 0.3s ease;
+    }
+
+    .page-counter {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .page-counter > div {
+        margin: 2%;
     }
 
 
--- portal/src/stores/posts.js
+++ portal/src/stores/posts.js
@@ -1,24 +1,50 @@
 import { defineStore } from 'pinia'
-import axios from 'axios'
+import axios, { all } from 'axios'
+
+const limit = 10
 
 export const usePostsStore = defineStore('posts', {
   state: () => ({
     posts: [],
     loading: false,
     error: null,
+    total_pages: 0,
+    page: 1,
+    allPosts: []
   }),
+
 
   actions: {
     async fetchPosts() {
       try {
-        const response = await axios.get('http://127.0.0.1:5000/api/posts')
-        this.posts = response.data
+        const response = await axios.get('http://127.0.0.1:5000/api/posts/')
+            
+        this.allPosts = response.data
+        this.total_pages = Math.ceil(this.allPosts.length / limit)
+        
+        const start = (this.page - 1) * limit
+        const end = start + limit
+        this.posts = this.allPosts.slice(start, end)
 
       } catch (error) {
-        this.error = 'Error al obtener las publicaciones'
+        this.error = error
       } finally {
         this.loading = false
       }
     },
+
+    async changePage() {
+      this.loading = true
+      this.page++
+      await this.fetchPosts()
+    },
+
+    async goBackPage() {
+      this.loading = true
+      this.page--
+      await this.fetchPosts()
+    },
   },
+
+  
 })
