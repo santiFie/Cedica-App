c238921ab9eb841385c86281cef1cd81e6149c48
luciocolli <luciocolli1@gmail.com>
2024-11-18 11:49:29 -0300

    Se agrega validacion de captcha a formulario de contacto en portal

--- portal/src/components/Contact.vue
+++ portal/src/components/Contact.vue
@@ -1,5 +1,5 @@
 <script>
-import { ref } from 'vue'
+import { ref, onMounted, onUnmounted } from 'vue'
 
 // esta es la funcion que esta en 'stores' que se encarga de enviar los datos a la api
 import { useContactStore } from '@/stores/contact'
@@ -11,18 +11,54 @@ export default {
     const lastName = ref('')
     const email = ref('')
     const message = ref('')
+    const captchaToken = ref('')
     const contactStore = useContactStore()
 
+    // se carga el script del captcha dinamicamente
+    const loadCaptchaScript = () => {
+      if (!document.getElementById('recaptcha-script')) {
+        const script = document.createElement('script');
+        script.id = 'recaptcha-script';
+        script.src = 'https://www.google.com/recaptcha/api.js';
+        script.async = true;
+        script.defer = true;
+        document.head.appendChild(script);
+      }
+    };
+
     const submitForm = () => {
+
+      if(!captchaToken.value) {
+        alert('Por favor, complete el captcha')
+        return
+      }
+
       const contactData = {
         name: name.value,
         last_name: lastName.value,
         email: email.value,
         message: message.value,
+        captcha: captchaToken.value,
       }
       // submitContact es la funcion del store que envia los datos del formulario
       contactStore.submitContact(contactData)
     }
+
+    const onCaptchaSuccess = (token) => {
+        captchaToken.value = token  // guardo el token generado por el captcha
+      }
+
+    onMounted(() => {
+      loadCaptchaScript(); // Carga el script al montar el componente
+
+      // Define onCaptchaSuccess en el objeto global para que sea accesible por el navegador
+      window.onCaptchaSuccess = onCaptchaSuccess;
+    });
+
+    onUnmounted(() => {
+      // Limpia la referencia global al desmontar el componente
+      delete window.onCaptchaSuccess;
+    });
 
     // se retornan las variables y la funcion que se usaran en el template
     return {
@@ -31,6 +67,7 @@ export default {
       email,
       message,
       submitForm,
+      onCaptchaSuccess,
     }
   },
 }
@@ -58,9 +95,15 @@ export default {
       <label for="message">Mensaje:</label>
       <textarea id="message" v-model="message" required></textarea>
     </div>
+    <div class="g-recaptcha" 
+         data-sitekey="6LeGsYIqAAAAAHtkb8P22Xoyfc-lyulQsGU3Ryx_" 
+         data-callback="onCaptchaSuccess">
+    </div>
     <button type="submit">Enviar</button>
   </form>
 </template>
+
+
 
 <style scoped>
 form {
--- portal/src/stores/contact.js
+++ portal/src/stores/contact.js
@@ -5,8 +5,6 @@ import js from '@eslint/js'
 export const useContactStore = defineStore('contactStore', {
   actions: {
     async submitContact(contactData) {
-        console.log('ADENTRO DE LA FUNCION SUBMIT CONTACT EN STORE')
-        console.log('CONTACT DATA:', contactData)
       try {
         // convertir contactData en un objeto JSON y enviar
         const response = await axios.post('http://127.0.0.1:5000/api/contacts/register', contactData, {
