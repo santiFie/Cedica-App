4ad957acf45cf8c5368ce40460b293833d967ccf
SANTIAGO FIERRO <santiago.fierro99@gmail.com>
2024-11-19 18:13:00 -0300

    Se realizan estilos

--- portal/src/components/Contact.vue
+++ portal/src/components/Contact.vue
@@ -6,6 +6,13 @@ import { useContactStore } from '@/stores/contact'
 
 export default {
   name: 'Contact',
+  methods: {
+      adjustHeight(event) {
+      const textarea = event.target;
+      textarea.style.height = 'auto';  // Restablece la altura antes de recalcularla
+      textarea.style.height = `${textarea.scrollHeight}px`;  // Ajusta la altura al contenido
+      }
+    },
   setup() {
     const name = ref('')
     const lastName = ref('')
@@ -48,6 +55,8 @@ export default {
         captchaToken.value = token  // guardo el token generado por el captcha
       }
 
+    
+
     onMounted(() => {
       loadCaptchaScript(); // Carga el script al montar el componente
 
@@ -78,22 +87,20 @@ export default {
     <!-- Defino el formulario con los campos necesarios
         Cuando se haga submit se ejecutara la funcion submitForm  
     -->
+  <h2 class="contact-title">Contactanos</h2>
+
   <form @submit.prevent="submitForm">
-    <div>
-      <label for="name">Nombre:</label>
-      <input id="name" v-model="name" type="text" required />
+    <div class="form-field">
+      <input id="name" class="form-input" v-model="name" type="text" placeholder="Nombre" required />
     </div>
-    <div>
-      <label for="lastName">Apellido:</label>
-      <input id="lastName" v-model="lastName" type="text" required />
+    <div class="form-field">
+      <input id="lastName" class="form-input" v-model="lastName" type="text" placeholder="Apellido" required />
     </div>
-    <div>
-      <label for="email">Email:</label>
-      <input id="email" v-model="email" type="email" required />
+    <div class="form-field">
+      <input id="email" class="form-input" v-model="email" type="email" placeholder="Correo Electrónico" required />
     </div>
-    <div>
-      <label for="message">Mensaje:</label>
-      <textarea id="message" v-model="message" required></textarea>
+    <div class="form-field">
+      <textarea id="message" class="form-message" v-model="message" placeholder="Mensaje" required @input="adjustHeight"></textarea>
     </div>
     <div class="g-recaptcha" 
          data-sitekey="6LeGsYIqAAAAAHtkb8P22Xoyfc-lyulQsGU3Ryx_" 
@@ -106,13 +113,75 @@ export default {
 
 
 <style scoped>
+
+.contact-title {
+  color: black;
+  margin-left: 6%;
+  margin-top: 5%;
+  font-size: 2em;
+  margin-bottom: 0.5%;
+  font-family: 'Roboto', sans-serif;
+}
+
 form {
   display: flex;
   flex-direction: column;
   gap: 10px;
+  padding: 6%;
+  margin-bottom: 5%;
+  margin-left: 5%;
+  margin-right: 5%;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(201, 169, 169, 0.1);
 }
 
+.form-field {
+  display: flex;
+  padding: 13px;
+  width: 100%;
+}
+
+.form-input {
+  padding: 13px;
+  border: 1px solid #ccc;
+  width: 100%;
+  font-size: inherit;
+}
+
+textarea {
+  font-family: sans-serif;
+  font-size: inherit;
+  padding: 13px;
+  border: 1px solid #ccc;
+  width: 100%;
+  font-size: inherit;
+  resize: vertical; 
+  overflow-y: hidden; 
+}
 button {
   margin-top: 10px;
+  width: 10%;
+  text-align: center;
+  font-size: inherit;
+  justify-content: center;
+  align-self: center;
+  padding: 8px;
+  background-color: #385668;
+  border: none;
+  border-radius: 5%;
+  color: white;
 }
+
+button:hover {
+  background-color: #2b3b4e;
+}
+
+.g-recaptcha {
+  margin-top: 10px;
+  margin-bottom: 10px;
+  display: flex;
+  justify-content: center;
+}
+
 </style>
