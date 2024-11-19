35621b9a2eb7cc949c44e347034c4350faf8e8a5
SANTIAGO FIERRO <santiago.fierro99@gmail.com>
2024-11-19 13:10:00 -0300

    Se reestablece configuración de variables de google para que se usen las variables de entorno

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -46,11 +46,6 @@ def create_app(env="production", static_folder="../../static"):
     routes.register(app)
     # Init OAuth
     configure_oauth(app)
-
-    print(app.config.get("GOOGLE_CLIENT_ID"))
-    print(app.config.get("GOOGLE_CLIENT_SECRET"))
-    print(app.config.get("GOOGLE_REDIRECT_URI"))
-
     # Error handlers
     errors.register_errors(app)
     
